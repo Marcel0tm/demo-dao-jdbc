56775c86d262a9b64fadef07d00f5a2cc3c919c1
Talisson Marcelo <marcelosilvatmls@gmail.com>
2023-02-26 22:37:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dp = new Department(1, "Cars");
-		System.out.println(dp);
+		
+		
+		Seller seller = new Seller(21, "Bob G.", "bob@gmail.com", new Date(), 3000.00, dp);
+	
+		System.out.println(seller);
 	}
 }
