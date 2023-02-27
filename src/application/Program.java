cea973ab4e6ce87dd87faec3d41dac1d5fcdea70
Talisson Marcelo <marcelosilvatmls@gmail.com>
2023-02-26 22:16:42 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dp = new Department(1, "Cars");
+		System.out.println(dp);
+	}
+}
